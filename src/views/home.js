d1dc120df18bab0089d8e5dd9e946b258dc7dfaa
ivvnv <ivana.rivera.c@gmail.com>
2020-08-22 13:17:43 -0400

    First pages commit

---
+++ src/views/home.js
@@ -0,0 +1,35 @@
+export default () => {
+  const viewsHome = /*html*/ `
+
+  
+
+  <header class="py-5 bg-image-full" style="background-image: url('img/banner1.jpg');">
+  <p class="text-over">EL SISTEMA AUTOMATIZADO
+  MÁS INNOVADOR DEL MERCADO, DONDE TODOS CUENTAN</p>
+  </header>
+
+
+  <div class="container">
+        <div class="row" style="margin:50px 0 50px 0;">
+            <div class="col" id="text-style">
+                EqualCredit te permite acceder a créditos de manera igualitaria; evaluamos tus datos reales sin importar quién eres y de manera automática te indicaremos si puedes optar a este beneficio, el cual permitirá abrirte puertas a todo lo que deseas.
+            </div>
+            <div class="col">
+                VIDEO
+            </div>
+        </div>
+    </div>
+
+   
+  <div class="py-5 bg-image-full" style="background-image: url('img/home_image.jpeg');">
+    <a href="#/credit" class="btn btn-info button_class">Pide tu crédito</a>
+    <div style="height: 200px;"></div>
+  </div> 
+
+  
+    `;
+    
+  const divElement = document.createElement("div");
+  divElement.innerHTML = viewsHome;
+  return divElement;
+};
